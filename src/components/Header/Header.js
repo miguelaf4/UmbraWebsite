96249ca2abf9aa3ad9519ecd6cbda031eb6dbb31
Miguel Arrañaga <miguelaf44@gmail.com>
2021-04-26 22:26:16 -0500

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <h1>Logo</h1>
+            <h2>Inicio</h2>
+        </div>
+    )
+}
+
+export default Header
