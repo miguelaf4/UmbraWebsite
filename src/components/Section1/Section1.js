3fbde1bd444f9626ef1e6934510ed180b1d81644
Alberto <albertogarciafigueroam@gmail.com>
2021-04-28 11:01:05 -0500

    Preparing Section1

---
+++ src/components/Section1/Section1.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './style.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Section1 = () => {
+    return (
+        <div className="box">
+            <div>one</div>
+            <div>two</div>
+        </div>
+    )
+}
+
+export default Section1
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,14 @@ import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Header from '../components/Header/Header'
+import Secction1 from "../components/Section1/Section1"
+import Section1 from "../components/Section1/Section1"
 
 
 const IndexPage = () => (
   <Layout>
     <Header/>
+    <Section1/>
     <h1>Umbra</h1>
     
   </Layout>
