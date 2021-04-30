8afb357d0bcfa29c589ce55e2d7b218af19087fd
Alberto <albertogarciafigueroam@gmail.com>
2021-04-30 18:34:08 -0500

    Form almost complete

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,11 +6,12 @@ const Form = () => {
     return (
         <div className="first-container">
             <div className="form-container">
-                <h3>Es momento de obtener tu lugar verdadero</h3>  
+                <h1>Es momento de obtener tu lugar verdadero</h1>  
                 <br/>
                 <p>
                 Llena el fomrulario para recibir más información.
                 </p>
+                <br/>
                 <form action="https://formsubmit.co/c2bd1a406c5ededf22c82068a3d714bf" method="POST">
                     <select id='depto_form' name='Departamento de interés'>
                         <option disabled selected>¿Cuál departamento te interesa</option>
@@ -18,10 +19,10 @@ const Form = () => {
                         <option value='2'>2</option>
                         <option value='3'>3</option>
                     </select>
-                    <input type="text" name='Nombre Cliente' id='nombre' placeholder='Nombre Completo'></input>
-                    <input type='email' name='Correo electrónico' id='email' placeholder='Email'></input>
-                    <input type="number" name='Teléfono/Whatsapp' id='phoneNumber' placeholder='Whatsapp'></input>
-                    <input type="submit"></input>
+                    <input type="text" class='fullname' name='Nombre Cliente' id='nombre' placeholder='Nombre Completo'></input>
+                    <input type="number" class='whats' name='Teléfono/Whatsapp' id='phoneNumber' placeholder='Whatsapp'></input>
+                    <input type='email' class='mail' name='Correo electrónico' id='email' placeholder='Email'></input>
+                    <input type="submit" class='envio'></input>
                 </form>
             </div>
             <div className="photo">
