d6b8ec6319ae313c0ba7ae06a0c6bea1af07ab5d
Alberto <albertogarciafigueroam@gmail.com>
2021-04-30 19:10:59 -0500

    Form fixed details

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -14,15 +14,15 @@ const Form = () => {
                 <br/>
                 <form action="https://formsubmit.co/c2bd1a406c5ededf22c82068a3d714bf" method="POST">
                     <select id='depto_form' name='Departamento de interés'>
-                        <option disabled selected>¿Cuál departamento te interesa</option>
+                        <option disabled selected class='ejemplo'>¿Cuál departamento te interesa</option>
                         <option value='1'>1</option>
                         <option value='2'>2</option>
                         <option value='3'>3</option>
                     </select>
                     <input type="text" class='fullname' name='Nombre Cliente' id='nombre' placeholder='Nombre Completo'></input>
-                    <input type="number" class='whats' name='Teléfono/Whatsapp' id='phoneNumber' placeholder='Whatsapp'></input>
+                    <input type="tel" class='whats' name='Teléfono/Whatsapp' id='phoneNumber' placeholder='Whatsapp'></input>
                     <input type='email' class='mail' name='Correo electrónico' id='email' placeholder='Email'></input>
-                    <input type="submit" class='envio'></input>
+                    <input type="submit" class='envio' value="ENVIAR"></input>
                 </form>
             </div>
             <div className="photo">
