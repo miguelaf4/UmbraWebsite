b5f220f07ccfabd78c2beb8f3af75c761a7177f9
Miguel Arrañaga <miguelaf44@gmail.com>
2021-05-31 18:46:39 -0500

    Final issues

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className="hero-container">
             <h1 className="hero-title">FIND YOUR <br/> TRUE PLACE.</h1>
             <div className="btn-container">
-                <a href="#concepto" className="btn btn-light" >CONOCE UMBRA</a>
+                <Link to="concepto" className="btn btn-light" spy={true} smooth={true} duration={1000} offset={-80}>CONOCE UMBRA</Link>
                 <Link to="contacto" className="btn btn-dark" spy={true} smooth={true} duration={1000} offset={-80}>CONTACTANOS</Link>
             </div>
             
--- src/components/Lifestyle/Lifestyle.js
+++ src/components/Lifestyle/Lifestyle.js
@@ -76,7 +76,7 @@ const Lifestyle = () => {
             </Slide> */}
             
             <Slider />
-
+            
             </div>
             
         </div>
